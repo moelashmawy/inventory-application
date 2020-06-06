2f22f619c79ecf78db179bac21278b1add731bc7
hamohuh <hamodroid@gmail.com>
2020-06-06 09:30:40 +0200

    Added server side form validation

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,75 +7,120 @@ const Product = require("../models/ProductModel");
 
 // hanle GET request at /api/category to get all the categories
 exports.categoryIndex = (req, res) => {
-  Category.find().then((category) => res.json(category));
+    Category.find().then(category => res.json(category));
 };
 
 //handle POST reqest at /api/category/create to create a new category
-exports.createCategory = (req, res, next) => {
-  let newCategory = new Category({
-    name: req.body.name,
-    description: req.body.description,
-  });
+exports.createCategory = [
+    // Validate that the name field is not empty and minimum length 2
+    //and Sanitize (trim) the field so it's clear to be inserted
+    body("name", "category name required").isLength({ min: 2 }).trim().escape(),
 
-  // Check if Category with same name already exists.
-  Category.findOne({ name: req.body.name }).exec(function (
-    err,
-    found_category
-  ) {
-    if (err) {
-      return next(err);
-    }
+    // Validate that the description field is not empty and minimum length 2
+    //and Sanitize (trim) the field so it's clear to be inserted
+    body("description", "category description required")
+        .isLength({ min: 10 })
+        .trim()
+        .escape(),
 
-    if (found_category) {
-      // Category exists, redirect to its page
-      res.redirect(found_category.url);
-    } else {
-      newCategory.save(function (err) {
-        if (err) {
-          return next(err);
+    // Continue process after validation
+    (req, res, next) => {
+        // Extract the validation errors from a request
+        const errors = validationResult(req);
+
+        // Create a newCategory object with escaped and trimmed data if it's valid
+        let newCategory = new Category({
+            name: req.body.name,
+            description: req.body.description
+        });
+
+        // if there are errors send them as json
+        if (!errors.isEmpty()) {
+            res.json(errors);
+        } else {
+            // Check if Category with same name already exists.
+            Category.findOne({
+                name: req.body.name
+            }).exec(function (err, found_category) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (found_category) {
+                    // Category exists, redirect to its page
+                    res.redirect(found_category.url);
+                } else {
+                    newCategory.save(function (err) {
+                        if (err) {
+                            return next(err);
+                        }
+                        // Category saved. Redirect to its page.
+                        res.redirect(newCategory.url);
+                    });
+                }
+            });
         }
-        // Category saved. Redirect to its page.
-        res.redirect(newCategory.url);
-      });
     }
-  });
-};
+];
 
 // handle GET request at /api/category/:id to get one category
 exports.categoryDetails = function (req, res) {
-  async.parallel(
-    {
-      category: function (callback) {
-        Category.findById(req.params.id).exec(callback);
-      },
-      product: function (callback) {
-        Product.find({ category: req.params.id }).exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) {
-        return res.json(err);
-      } else {
-        return res.json(results);
-      }
-    }
-  );
+    async.parallel(
+        {
+            category: function (callback) {
+                Category.findById(req.params.id).exec(callback);
+            },
+            product: function (callback) {
+                Product.find({
+                    category: req.params.id
+                }).exec(callback);
+            }
+        },
+        function (err, results) {
+            if (err) {
+                return res.json(err);
+            } else {
+                return res.json(results);
+            }
+        }
+    );
 };
 
 // handle DELETE request at /api/category/:id/delete
 exports.deleteCategory = (req, res) => {
-  Category.findByIdAndDelete(req.params.id)
-    .then(() => res.json({ message: `Category was deleted Succefully` }))
-    .catch((err) => res.json(err));
+    Category.findByIdAndDelete(req.params.id)
+        .then(() =>
+            res.json({
+                message: `Category was deleted Succefully`
+            })
+        )
+        .catch(err => res.json(err));
 };
 
 //handle PUT request at /api/category/:id/update to update a category with id
-exports.updateCategory = (req, res) => {
-  Category.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true, useFindAndModify: false }
-  )
-    .then((product) => res.json(product))
-    .catch((err) => res.json(err));
-};
+exports.updateCategory = [
+    body("name").isLength({ min: 2 }).trim().escape(),
+
+    body("description").isLength({ min: 10 }).trim().escape(),
+
+    (req, res) => {
+        const errors = validationResult(req);
+
+        let updatedCategory = new Category({
+            name: req.body.name,
+            description: req.body.description,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            res.json(errors);
+        } else {
+            Category.findByIdAndUpdate(req.params.id, updatedCategory, {
+                new: true,
+                useFindAndModify: false
+            })
+                .then(cat => res.json(cat))
+                .catch(err => res.json(err));
+        }
+    }
+];
--- views/src/components/AddCategoryForm.js
+++ views/src/components/AddCategoryForm.js
@@ -7,79 +7,85 @@ import { useDispatch } from "react-redux";
 
 // form validation useing Yup
 const validate = () =>
-  Yup.object({
-    name: Yup.string()
-      .min(2, "Must be more then one character")
-      .required("Category name is required"),
-    description: Yup.string()
-      .min(10, "Must be more than 10 characters")
-      .required("This field is required"),
-  });
+    Yup.object({
+        name: Yup.string()
+            .min(2, "Must be more then one character")
+            .required("Category name is required"),
+        description: Yup.string()
+            .min(10, "Must be more than 10 characters")
+            .required("This field is required")
+    });
 
 function AddCategoryForm() {
-  const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-  // handle modal show and hide
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    // handle modal show and hide
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  // dispatch our redux action
-  const dispatch = useDispatch();
+    // dispatch our redux action
+    const dispatch = useDispatch();
 
-  return (
-    <div>
-      <Button variant="primary" onClick={handleShow}>
-        Add Category
-      </Button>
+    return (
+        <div>
+            <Button variant="primary" onClick={handleShow}>
+                Add Category
+            </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add New Category</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Formik
-            initialValues={{
-              name: "",
-              description: "",
-            }}
-            validationSchema={validate}
-            onSubmit={(values, { setSubmitting }) => {
-              const newCategory = {
-                name: values.name,
-                description: values.description,
-              };
-              dispatch(addCategory(newCategory));
-              handleClose();
-              setSubmitting(false);
-            }}
-          >
-            <Form>
-              <div className="form-group">
-                <label htmlFor="name">name</label>
-                <Field type="text" name="name" className="form-control" />
-                <ErrorMessage component={Toast} name="name" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">description</label>
-                <Field
-                  as="textarea"
-                  name="description"
-                  className="form-control"
-                />
-                <ErrorMessage component={Toast} name="description" />
-              </div>
-              <Button variant="primary" type="submit">
-                Submit{" "}
-              </Button>{" "}
-              <Button variant="secondary" onClick={handleClose}>
-                Close{" "}
-              </Button>{" "}
-            </Form>
-          </Formik>
-        </Modal.Body>
-      </Modal>
-    </div>
-  );
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add New Category</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        initialValues={{
+                            name: "",
+                            description: ""
+                        }}
+                        validationSchema={validate}
+                        onSubmit={(values, { setSubmitting }) => {
+                            const newCategory = {
+                                name: values.name,
+                                description: values.description
+                            };
+                            dispatch(addCategory(newCategory));
+                            handleClose();
+                            setSubmitting(false);
+                        }}>
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="name">name</label>
+                                <Field
+                                    type="text"
+                                    name="name"
+                                    className="form-control"
+                                />
+                                <ErrorMessage component={Toast} name="name" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="description">description</label>
+                                <Field
+                                    as="textarea"
+                                    name="description"
+                                    className="form-control"
+                                />
+                                <ErrorMessage
+                                    component={Toast}
+                                    name="description"
+                                />
+                            </div>
+                            <Button variant="primary" type="submit">
+                                Submit{" "}
+                            </Button>{" "}
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close{" "}
+                            </Button>{" "}
+                        </Form>
+                    </Formik>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
 }
 
 export default AddCategoryForm;
