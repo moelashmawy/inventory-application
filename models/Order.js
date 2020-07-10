fe68c8338667d2a89751fbc3e33dc303b34bac48
hamohuh <hamodroid@gmail.com>
2020-07-11 00:28:55 +0200

    Added Shipping and Delivery date

--- models/Order.js
+++ models/Order.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   orderDate: { type: Date, default: Date.now },
+  shippedDate: { type: Date },
+  deliveredDate: { type: Date },
   products: { type: Array, default: [] },
   totalPrice: { type: Number },
   address: { type: Schema.Types.ObjectId, ref: "Address" }
