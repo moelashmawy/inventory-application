15253d018d44a375c263ff939e4f6386062bbee4
hamohuh <hamodroid@gmail.com>
2020-08-18 19:58:04 +0200

    Refactored code to cloudinary images

--- middlewares/handleImageMulter.js
+++ middlewares/handleImageMulter.js
@@ -1,6 +1,38 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// Multer handling image upload Middleware at /api/product/create
+cloudinary.config({
+  cloud_name: "hamohuh",
+  api_key: "838736889631699",
+  api_secret: "4V8gR-toRb1FCVmfmD5nM-mGL5M"
+});
+
+var storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "E-commerce",
+    allowed_formats: ["jpg", "png", "jpeg", "gif"] // supports promises as well
+  }
+});
+
+exports.productImages = function () {
+  const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+  return upload.array("productImage", 12);
+};
+
+// in case for images local storing
+/* // Multer handling image upload Middleware at /api/product/create
 exports.productImages = function () {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -24,3 +56,4 @@ exports.productImages = function () {
 
   return upload.array("productImage", 12);
 };
+ */
