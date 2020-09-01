bd2ca971fb4cbce608a83bcbadeae22c2b24cddf
hamohuh <hamodroid@gmail.com>
2020-09-01 08:07:38 +0200

    Edited Readme

--- README.md
+++ README.md
@@ -125,7 +125,7 @@ $ npm run dev (for both client and server side)
 - Twitter: [@hamohuh](https://twitter.com/hamohuh)
 - Github: [@hamohuh](https://github.com/hamohuh)
 - Linkedin: [@mo-elashmawy](https://www.linkedin.com/in/mo-elashmawy/)
-- Email: [hamodroid@gmail.com](mailto:hamodroid@gmail.com)
+- Email: [hamohuh@gmail.com](mailto:hamohuh@gmail.com)
 
 ## 📝 &nbsp; License
 
--- views/src/index.js
+++ views/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import store from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
